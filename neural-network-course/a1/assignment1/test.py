d4f1db7d331c1d46d2ef1eaba8e3ad4b16fb3253
trongr <devgruxcon@gmail.com>
2016-07-18 09:49:06 -0400

    neural networks: mnist test data for tsne

--- neural-network-course/a1/assignment1/test.py
+++ neural-network-course/a1/assignment1/test.py
@@ -1,5 +1,6 @@
 import cPickle
 import checking
+import language_model as lm
 
 def load_data():
     data = cPickle.load(open('data.pk', 'rb'))
@@ -8,4 +9,7 @@ def load_data():
 def test_gradient():
     checking.check_gradients()
 
-test_gradient()
+def train():
+    model = lm.train(16, 128)
+
+train()
