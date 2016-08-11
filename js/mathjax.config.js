f2b808494b83b292b7711050fc1b0cde18d2e8af
trongr <devgruxcon@gmail.com>
2016-08-10 22:43:02 -0400

    more notes and ex

--- js/mathjax.config.js
+++ js/mathjax.config.js
@@ -9,8 +9,30 @@ window.MathJax = {
     "HTML-CSS": {
         availableFonts: ["TeX"],
     },
+    SVG: {
+        // font: "STIX-Web"
+    },
     TeX: {
+        equationNumbers: { autoNumber: "AMS" },
         Macros: {
+
+            def: "\\stackrel{\\text{df}}{=}",
+            bar: ["\\overline{#1}", 1],
+
+            span: "\\mathop{\\rm span}",
+            tr: "\\mathop{\\rm tr}",
+            char: "\\mathop{\\rm char}",
+
+            Sum: "\\sum\\limits",
+            Prod: "\\prod\\limits",
+            Lim: "\\lim\\limits",
+            Cup: "\\bigcup\\limits",
+            Cap: "\\bigcap\\limits",
+            count: "\\mathop{\\rm count}\\limits",
+            textsum: "\\mathop{\\rm sum}\\limits",
+
+            0: "\\varnothing",
+
             a: "\\alpha",
             b: "\\beta",
             d: "\\delta",
@@ -28,20 +50,11 @@ window.MathJax = {
             D: "\\Delta",
             L: "\\Lambda",
 
+            BB: "\\mathbf B",
             CC: "\\mathbf C",
             NN: "\\mathbf N",
             QQ: "\\mathbf Q",
             RR: "\\mathbf R",
-
-            span: "\\mathop{\\rm span}",
-            tr: "\\mathop{\\rm tr}",
-
-            Sum: "\\sum\\limits",
-            Prod: "\\prod\\limits",
-            Lim: "\\lim\\limits",
-            count: "\\mathop{\\rm count}\\limits",
-            textsum: "\\mathop{\\rm sum}\\limits",
-
         }
     }
 }
