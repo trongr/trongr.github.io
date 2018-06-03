88ff826e52d0e18ed619dfea547e8e84f25127ac
trong <devgruxcon@gmail.com>
2018-06-03 15:50:27 -0400

    Autogen TOC

--- js/global.js
+++ js/global.js
@@ -1,22 +1,31 @@
-(function(){
-    $(window).on("load", function(e){
+(function () {
+    $(window).on("load", function (e) {
         smartquotes()
         escapeHTMLInCodeBlocks()
         highlightPreCode()
         toggleConsole()
+        initTOC()
     })
 
-    function toggleConsole(){
+    function initTOC() {
+        try {
+            $('#toc').toc();
+        } catch (error) {
+            console.error("Cannot generate Table of Contents", error)
+        }
+    }
+
+    function toggleConsole() {
         try {
             var show = purl(window.location.href).param("console") == "1"
             CLI.toggleConsole(show)
-        } catch (er){
+        } catch (er) {
             console.log("WARNING. toggleConsole: most likely lib not defined:", er)
         }
     }
 
-    function highlightPreCode(){
-        $('pre code').each(function(i, block) {
+    function highlightPreCode() {
+        $('pre code').each(function (i, block) {
             hljs.highlightBlock(block);
         });
     }
@@ -27,8 +36,8 @@
     // code listings are pasted in data-content of
     // <code data-content=''></code> instead of inside the tag, so we
     // can preprocess and escape div tags, e.g. for HTML code.
-    function escapeHTMLInCodeBlocks(){
-        $("code").each(function(){
+    function escapeHTMLInCodeBlocks() {
+        $("code").each(function () {
             if (!$(this).attr("data-content")) return
             var content = $(this).attr("data-content")
                 .replace(/</g, '&lt;')
