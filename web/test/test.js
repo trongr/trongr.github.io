a17f81d2f09b7939d6cd4090f4bd29e8ce1685a6
trong <devgruxcon@gmail.com>
2018-12-16 04:33:41 -0500

    Notes

--- web/test/test.js
+++ web/test/test.js
@@ -1,4 +1,19 @@
-new Promise((resolve) => {
-  console.log("resolving")
-  resolve()
-}).then(null)
+const test = async () => {
+  try {
+    const collections = [1, 2, 3, 4]
+    await collections
+      .map((collection) => () =>
+        new Promise(async (resolve, reject) => {
+          setTimeout(() => {
+            console.log("resolving", collection)
+            reject(new Error(`Testing error ${collection}`))
+          }, 2000)
+        }),
+      )
+      .reduce((acc, fn) => acc.then(fn), Promise.resolve())
+  } catch (e) {
+    console.log("it's an error")
+  }
+}
+
+test()
